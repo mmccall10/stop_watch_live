6042e04f409d5b144425de6d740ba7309851c484
Mike McCall <pyramind10@gmail.com>
2019-04-05 23:46:18 -0700

    working version

--- lib/stop_watch_web/live/stop_watch_live.ex
+++ lib/stop_watch_web/live/stop_watch_live.ex
@@ -1,7 +1,7 @@
 defmodule StopWatchWeb.StopWatchLive do
   use Phoenix.LiveView
 
-  @state [timer: 0, started: false, laps: []]
+  @state [timer: 0, started: false, laps: [], started_at: nil]
 
   def mount(_session, socket) do
     {:ok, assign(socket, @state)}
@@ -12,24 +12,35 @@ defmodule StopWatchWeb.StopWatchLive do
   end
 
   def handle_event("start", _, socket) do
-    Process.send_after(self(), :update_time, 10)
-
-    {:noreply, assign(socket, started: true)}
+    Process.send_after(self(), :update_timer, 10)
+    started_at = System.os_time(:milliseconds) - socket.assigns.timer
+    {:noreply, assign(socket, started: true, started_at: started_at)}
   end
 
   def handle_event("stop", _, socket) do
     {:noreply, assign(socket, started: false)}
   end
 
-  def handle_info(:update_time, socket) do
-    case socket.assigns.started do
-      true ->
-        Process.send_after(self(), :update_time, 10)
-        timer = socket.assigns.timer + 10
-        {:noreply, assign(socket, timer: timer)}
+  def handle_event("reset", _, socket) do
+    {:noreply, assign(socket, timer: 0, started: false, started_at: nil, laps: [])}
+  end
 
-      _ ->
-        {:noreply, socket}
+  def handle_event("lap", _, socket) do
+    lap_num = Enum.count(socket.assigns.laps) + 1
+    os_time = System.os_time(:milliseconds)
+    time =  os_time - socket.assigns.started_at
+    laps = [{lap_num, time}] ++ socket.assigns.laps
+    {:noreply, assign(socket, laps: laps, timer: 0, started_at: os_time)}
+  end
+
+  def handle_info(:update_timer, socket) do
+
+    if socket.assigns.started do
+      Process.send_after(self(), :update_timer, 10)
+      timer = System.os_time(:milliseconds) - socket.assigns.started_at
+      {:noreply, assign(socket, timer: timer)}
+    else
+      {:noreply, socket}
     end
   end
 end
--- lib/stop_watch_web/views/page_view.ex
+++ lib/stop_watch_web/views/page_view.ex
@@ -1,3 +1,36 @@
 defmodule StopWatchWeb.PageView do
   use StopWatchWeb, :view
+
+  def format_timer(timer) do
+    min = div(timer, 60000) |> to_string() |> String.pad_leading(2, ["0"])
+    seconds = timer |> rem(60000) |> div(1000) |> to_string() |> String.pad_leading(2, ["0"])
+    milliseconds = timer |> rem(1000) |> div(10) |> to_string() |> String.pad_leading(2, ["0"])
+    "#{min}:#{seconds}:#{milliseconds}"
+  end
+
+  def speed_class(val, laps) do
+    cond do
+      val == fastest_time(laps) -> "fast"
+      val == slowest_time(laps) -> "slow"
+      true -> "neutral"
+    end
+  end
+
+  def slowest_time(laps) do
+    Enum.reduce(laps, 0, fn {_, x}, acc ->
+      case x > acc do
+        true -> x
+        _ -> acc
+      end
+    end)
+  end
+
+  def fastest_time(laps) do
+    Enum.reduce(laps, 999_999_999_999, fn {_, x}, acc ->
+      case x < acc do
+        true -> x
+        _ -> acc
+      end
+    end)
+  end
 end
